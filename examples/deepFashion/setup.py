73658d12aea11f14df392aed35f1e69d6bab130c
SiddhantManocha <siddhantmanocha1994@gmail.com>
2016-03-10 20:51:40 +0530

    added parse args to setup

--- examples/deepFashion/setup.py
+++ examples/deepFashion/setup.py
@@ -4,6 +4,52 @@ from os import listdir
 from os.path import isfile, join
 import json
 import re
+import optparse
+
+def parse_args():
+
+	usage="""
+		use flag --name with arguement to specify project name 
+		use flag --root to specify the root folder(empty for default)
+		use flag --local to specify the local root folder(empty for default)
+		use flag --db to specify the directory for the database
+
+	"""
+	parser = optparse.OptionParser(usage)
+
+	help = "Name of the Project, default None"
+	parser.add_option('--name', default=None,help=help)
+
+	help = "root folder for the project, default /home/ubuntu/caffe-cvprw15/examples/deepFashion"
+	parser.add_option('--root', help=help, default='/home/ubuntu/caffe-cvprw15/examples/deepFashion')
+
+	help = "local folder for the project, default /home/siddhantmanocha/Projects/...."
+	parser.add_option('--local', help=help, default='/home/siddhantmanocha/Projects/neural/deepFashion/examples/deepFashion')
+
+	help = "db folder for the project, default /data/deepfashion"
+	parser.add_option('--db', help=help, default='/data/deepfashion')
+
+	options, args = parser.parse_args()
+
+	if options.root[-1]=='/':
+		options.root=options.root[:-1]
+
+	if options.local[-1]=='/':
+		options.local=options.local[:-1]
+
+	if options.db[-1]=='/':
+		options.db=options.db[:-1]
+
+	if (os.path.isdir(options.root)):
+		pass
+	else:
+		parser.error('Project Root Directory Does Not Exist')
+
+	if not options.name:
+	    parser.error('Name is required')
+
+	return options
+
 
 def findAndReplace(filename,find,replace):
 	with open(filename, 'r') as content_file:
@@ -54,12 +100,12 @@ def protxtFiles(PROJECT):
 
 
 
-def main():
-	PROJECT_ROOT="/home/ubuntu/caffe-cvprw15/examples/deepFashion"
-	LOCAL_PROJECT_ROOT="/home/siddhantmanocha/Projects/neural/deepFashion/examples/deepFashion"
-	PROJECT_NAME="Test"
-	DBDIR="/data/deepfashion/"
-
+def main(options):
+	PROJECT_ROOT=options.root
+	LOCAL_PROJECT_ROOT=options.local
+	PROJECT_NAME=options.name
+	DBDIR=options.db
+	DBDIR=DBDIR+"/"
 	if (os.path.isdir(PROJECT_NAME)):
 		print "Project with same name exists"
 		return
@@ -78,8 +124,11 @@ def main():
 	configureJson(LOCAL_PROJECT_ROOT,PROJECT_NAME,DBDIR,'SETTINGS.json.local')
 	protxtFiles(PROJECT_NAME)
 
-main()
-print "Success"
+
+if __name__ == '__main__':
+	options=parse_args()
+	main(options)
+	print "Success"
 
 
 
