5b5a9a7ca1601f5b25cba4d6cf497b8e5c2d44e4
SiddhantManocha <siddhantmanocha1994@gmail.com>
2016-03-11 17:51:59 +0530

    added matlab wrapper for autoencoder

---
+++ matlab/caffe/prepare_batch_autoencoder.m
@@ -0,0 +1,29 @@
+% ------------------------------------------------------------------------
+function images = prepare_batch_autoencoder(image_files,batch_size)
+% ------------------------------------------------------------------------
+
+num_images = length(image_files);
+
+CROPPED_DIM = 50;
+
+num_images = length(image_files);
+images = zeros(CROPPED_DIM,CROPPED_DIM,3,batch_size,'single');
+
+parfor i=1:num_images
+    % read file
+    fprintf('%c Preparing %s\n',13,image_files{i});
+    try
+        im = imread(image_files{i});
+        % resize to fixed input size
+        im = single(im);
+        im = imresize(im, [CROPPED_DIM CROPPED_DIM], 'bilinear');
+        % Transform GRAY to RGB
+        if size(im,3) == 1
+            im = cat(3,im,im,im);
+        end
+        % Crop the center of the image
+        images(:,:,:,i) = im;
+    catch
+        warning('Problems with file',image_files{i});
+    end
+end
