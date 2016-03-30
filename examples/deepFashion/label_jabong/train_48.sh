5fbe78be9c65c19665af97f7e2fcc73f1427ce60
SiddhantManocha <siddhantmanocha1994@gmail.com>
2016-03-30 22:16:26 +0530

    added newer model file

--- examples/deepFashion/label_jabong/train_48.sh
+++ examples/deepFashion/label_jabong/train_48.sh
@@ -1,1 +1,1 @@
-../../../build/tools/caffe train -solver  solver_fashion_48.prototxt -weights ../../../models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel -gpu 0 2>&1 | tee log.txt
+/home/ubuntu/caffe/build/tools/caffe train -solver  new_solver.prototxt -weights ../../../models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel -gpu 0 2>&1 | tee log.txt
