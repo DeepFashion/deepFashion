75886b26e30525afc5aa1e6ebb08fa62cdff2367
Ubuntu <ubuntu@ip-172-31-47-233.ec2.internal>
2016-04-09 17:29:32 +0000

    minor

--- examples/deepFashion/f.py
+++ examples/deepFashion/f.py
@@ -1,4 +1,13 @@
-from scripts import predict
-embedding=predict.InputImagePredict('/home/ubuntu/caffe-cvprw15/examples/deepFashion/dataset/http:__static2.jassets.com_p_King-26-I-Mustard-Yellow-Solid-Short-1248-1335051-1-catalog_s.jpg','/home/ubuntu/caffe-cvprw15/examples/deepFashion/label_jabong/SETTINGS.json')
-nn=predict.getNN('/home/ubuntu/caffe-cvprw15/examples/deepFashion/label_jabong/SETTINGS.json', embedding)
-print nn
+import sys
+sys.path.append('/home/ubuntu/caffe-cvprw15/examples/deepFashion/scripts')
+import predict
+import getNN
+import urllib
+def compute(imageURL):
+	urllib.urlretrieve(imageURL, '/home/ubuntu/caffe-cvprw15/examples/deepFashion/tmp/0001.jpg')
+	embedding=predict.InputImagePredict('/home/ubuntu/caffe-cvprw15/examples/deepFashion/tmp/0001.jpg','/home/ubuntu/caffe-cvprw15/examples/deepFashion/label_jabong/SETTINGS.json')
+	nn=getNN.computeNN('/home/ubuntu/caffe-cvprw15/examples/deepFashion/label_jabong/SETTINGS.json', embedding)
+	return nn
+urlval="http://static2.jassets.com/p/Rider-Republic-Brown-Solid-Skirt-8202-3058971-1-catalog_s.jpg"
+print compute(urlval)
+
