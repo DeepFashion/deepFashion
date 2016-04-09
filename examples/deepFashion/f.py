c72ec69ff5c7fb87370e42d99e4691c73271fe64
SiddhantManocha <siddhantmanocha1994@gmail.com>
2016-04-09 23:01:26 +0530

    minor

--- examples/deepFashion/f.py
+++ examples/deepFashion/f.py
@@ -2,3 +2,4 @@ from scripts import predict
 embedding=predict.InputImagePredict('/home/ubuntu/caffe-cvprw15/examples/deepFashion/dataset/http:__static2.jassets.com_p_King-26-I-Mustard-Yellow-Solid-Short-1248-1335051-1-catalog_s.jpg','/home/ubuntu/caffe-cvprw15/examples/deepFashion/label_jabong/SETTINGS.json')
 nn=predict.getNN('/home/ubuntu/caffe-cvprw15/examples/deepFashion/label_jabong/SETTINGS.json', embedding)
 print nn
+
