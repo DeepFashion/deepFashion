1544b81056bf18fc6fd0f0ca4c82957a0600594c
SiddhantManocha <siddhantmanocha1994@gmail.com>
2016-04-11 22:49:04 +0530

    test

--- examples/deepFashion/scripts/exp.py
+++ examples/deepFashion/scripts/exp.py
@@ -2,11 +2,27 @@
 '''
 demostrate how to write a rpc server
 '''
+import json
+sys.path.append('/home/ubuntu/caffe-cvprw15/examples/deepFashion/scripts')
+import predictTags as predict
+import getNear
+import urllib
+import random
+import string
+
 import sys, os, uuid, time
 sys.path.append(os.path.abspath(".."))
 import time
 from haigha.connection import Connection
 from haigha.message import Message
+
+
+try:
+    classifier = predict.CreateClassifier(self.settings_file)
+    print "Creating caffe client"
+except:
+    print 'Some error'
+    assert(False)
 
 connection = Connection(
     user='vastrai', password='vastra1',
@@ -15,6 +31,36 @@ connection = Connection(
 )
 channel = connection.channel()
 channel.queue.declare(queue='rpc_queue', auto_delete=False)
+
+
+SETTINGS_FILE_EMBEDDING='/home/ubuntu/caffe-cvprw15/examples/deepFashion/label_jabong/SETTINGS.json'
+SETTINGS_FILE_TAGS='/home/ubuntu/caffe-cvprw15/examples/deepFashion/multimodal/SETTINGS.json'
+
+def computeNN(imageURL):
+    filename=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))+'.jpg'
+    filename='/home/ubuntu/caffe-cvprw15/examples/deepFashion/tmp/'+filename
+    if os.path.isfile(filename):
+        os.remove(filename) 
+    urllib.urlretrieve(imageURL, filename)
+    embedding=predict.InputImagePredict(filename,SETTINGS_FILE_EMBEDDING,"embedding",classifier)
+    result=getNear.computeNN(SETTINGS_FILE_EMBEDDING, embedding)
+    for i in range(len(result)):
+        result[i]=result[i].strip()
+        result[i]=result[i][8:]
+        result[i]=result[i].replace("_", "/")
+        result[i]=result[i].replace("catalog/s", "catalog_s")
+    return result
+
+
+def computeTags(imageURL):
+    filename=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))+'.jpg'
+    filename='/home/ubuntu/caffe-cvprw15/examples/deepFashion/tmp/'+filename
+    if os.path.isfile(filename):
+        os.remove(filename) 
+    urllib.urlretrieve(imageURL, filename)
+    tags=predict.InputImagePredict(filename,SETTINGS_FILE_TAGS,"tags",classifier)
+    return tags
+
 
 def fib(n):
     if n == 0:
@@ -25,17 +71,12 @@ def fib(n):
         return fib(n-1) + fib(n-2)
 
 def on_request(msg):
-    print msg
-    n = int(msg.body)
-    time.sleep(15)
-    print " [.] fib(%s)"  % (n,)
-    result = fib(n)
-
+    imageURL = msg.body
+    result=computeNN(imageURL)
     reply_to = msg.properties["reply_to"]
     correlation_id = msg.properties["correlation_id"]
-    resp = Message(str(result), correlation_id=correlation_id)
+    resp = Message(json.dumps(result), correlation_id=correlation_id)
     channel.basic.publish(resp,'',reply_to)
-
     delivery_info = msg.delivery_info
     channel.basic.ack(delivery_info["delivery_tag"])
 
